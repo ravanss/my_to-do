719839dbe491e780608afe487b376970f4d5547d
Saulo Pedro da Silva <psaulo7@gmail.com>
2025-10-15 18:17:04 -0300

    atualizando js de listagem

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,11 @@
 //itens do array
 let tasks = ["Item x","Item xx","Item xxx"];
 console.log(tasks);
+console.log(tasks[1]);
 
+//acessando o elemento ul
 const list = document.getElementById("tasks");
 console.log(list);
+
+//função para lista itens no front-end
 
