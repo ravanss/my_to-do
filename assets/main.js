a352ecb0ab345c7157a78cffe67ebf6807ad8307
unknown <psaulo7@gmail.com>
2025-10-19 00:08:17 -0300

    Alterando para localStorage

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-//itens do array tasks 
+/*itens do array tasks 
 let tasks = ["Item x","Item xx","Item xxx"];
 
 //seleciona a lista no DOM onde os itens serão inseridos dinamicamente
@@ -6,8 +6,20 @@ const list = document.getElementById("tasks");
 
 //função para lista itens no front-end da aplicação
 tasks.forEach(task => {
-    console.log(task);
     const li = document.createElement("li");
     li.textContent = task;
     list.appendChild(li);
-});
+});*/
+
+//Trocando para localStorage
+let tasks = [
+    { text: "Item x" },
+    { text: "Item xx" },
+    { text: "Item xxx" },
+];
+
+tasks.forEach(task => console.log(task));
+
+
+// Exemplo: Salvar sua lista atual
+// saveTasks(tasks);
