a045f509da222aeb6b3ed8daa9cc16f4262d947c
Saulo Pedro da Silva <psaulo7@gmail.com>
2025-10-22 17:19:38 -0300

    update do arquivo de main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
 //Array simples com informações padrões
-let tasks = ["Item x","Item xx","Item xxx"];
+let task = ["Item x","Item xx","Item xxx"];
 
 //Converta a matriz em uma string JSON
-const jsonArray = JSON.stringify(tasks);
+const tankJson = JSON.stringify(task);
 
 //Armazene a string JSON no localStorage com uma chave chamada "dados"
-localStorage.setItem("dados", jsonArray);
+localStorage.setItem("dados", tankJson);
 
 //Variavel para armazenar os dados recuperados apos verificação de retorno dos dados
-let retrievedArray = [];
+let tasks = [];
 
 //Recuperar a string JSON do localStorage
-const retrievedJson = localStorage.getItem("dados");
+const tasksJson = localStorage.getItem("dados");
 
 //Verifique se os dados existem antes de analisar para evitar erros
-if (retrievedJson) {
-    retrievedArray = JSON.parse(retrievedJson);
+if (tasksJson) {
+    tasks = JSON.parse(tasksJson);
 }
 
 //seleciona a lista no DOM onde os itens serão inseridos dinamicamente
 const list = document.getElementById("tasks-list");
 
 //função para lista itens no front-end da aplicação
-retrievedArray.forEach(task => {
+tasks.forEach(task => {
     const li = document.createElement("li");
     li.textContent = task;
     list.appendChild(li);
@@ -31,12 +31,12 @@ retrievedArray.forEach(task => {
 //Função para adicionar uma nova tarefa
 function addTask(){
     const taskInput = document.getElementById("task-input");
-    const newTask = taskInput.value.trim();
+    const newItem = taskInput.value.trim();
     // Verifica se o input não está vazio
-    if (newTask != ""){
+    if (newItem != ""){
         // Adiciona a nova tarefa no início do array
-        retrievedArray.unshift(newTask);
-        console.log(retrievedArray);
+        tasks.unshift(newItem);
+        console.log(tasks);
     } else {
         alert("Por favor, insira uma tarefa válida.");
     }
