78f229235ae612c65a79d10b626b4e3af4e80316
Saulo Pedro da Silva <psaulo7@gmail.com>
2025-11-01 00:27:09 -0300

    Função de alterar o nome da tarefa

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,21 @@ const list = document.getElementById("tasks-list");
 //Função para lista itens no front-end da aplicação ao carregar a página com loadTasks()
 window.onload = function() {
     tasks.forEach(task => {
+        //Criar elementos HTML para cada tarefa
         const li = document.createElement("li");
         const a = document.createElement("a");
+        const span = document.createElement("span");
+        const i = document.createElement("i");
+        //Configurar classes e atributos
+        i.className = "fa-solid fa-trash";
+        li.className = "list-item";
         a.textContent = task;
+        a.setAttribute("data-task", task);
+        //Criado a estrutura e inserido na lista
         list.appendChild(li);
         li.appendChild(a);
+        a.appendChild(span);
+        span.appendChild(i);
     })
 };
 
@@ -40,6 +50,9 @@ function addTask(){
         alert("Por favor, insira uma tarefa válida.");
     }
 }
+
+//Função para alterar o nome da tarefa
+
 
 //função para salvar as tarefas no localStorage
 function salveTasks(tasksArray){
