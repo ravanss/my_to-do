033ec43282847f51130114a3dea5cebd1a37aafc
Saulo Pedro da Silva <psaulo7@gmail.com>
2026-01-19 14:11:37 -0300

    conexao com db

--- php/config.php
+++ php/config.php
@@ -5,4 +5,12 @@ $username = 'root';
 $password = '';
 $database = 'to-do_app';
 
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+};
+
+?>
 
