fc2e06eeda1a6a1793238b01147cdb7e71c2063f
Saulo Pedro da Silva <psaulo7@gmail.com>
2026-01-20 15:30:44 -0300

    salvando dados no banco de dados

--- registra.php
+++ registra.php
@@ -7,6 +7,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['nome'];
     $email = $_POST['email'];
     $password = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+
+    try {
+        $stmt->execute([$username, $email, $password]);
+        echo "<script>alert('Registro bem-sucedido!'); window.location.href = 'app.php';</script>";
+    } catch (PDOException $e) {
+        echo "<script>alert('Erro ao registrar: " . $e->getMessage() . "');</script>";
+    };
 }
 ?>
 
@@ -17,13 +27,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <div class="col-12"></div>
                     <form action="" method="post">
                         <div class="input-group">
-                            <input type="text" name="nome" class="form-control" placeholder="Adicione seu nome">
+                            <input type="text" name="nome" class="form-control" placeholder="Adicione seu nome" required>
                         </div>
                         <div class="input-group">
-                            <input type="email" name="email" class="form-control" placeholder="Adicione seu email">
+                            <input type="email" name="email" class="form-control" placeholder="Adicione seu email" required>
                         </div>
                         <div class="input-group">
-                            <input type="password" name="senha" class="form-control" placeholder="Adicione sua senha">
+                            <input type="password" name="senha" class="form-control" placeholder="Adicione sua senha" required>
                         </div>
                         <div class="input-group">
                             <button type="submit" class="btn btn-primary">Registrar</button>
