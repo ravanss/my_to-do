4c18def0aeb51f0b3749f45c90900da8c5eddb97
Saulo Pedro da Silva <psaulo7@gmail.com>
2026-03-20 23:57:30 -0300

    alterando senha de usuario

--- reset.php
+++ reset.php
@@ -1,22 +1,10 @@
 <?php
 require_once 'php/config.php';
 
-// Imagine que você já recebeu o e-mail via POST
-/*$email = $_POST['email'];
-$novaSenhaPura = substr(bin2hex(random_bytes(4)), 0, 8); // Gera algo como 'a1b2c3d4'
-$hash = password_hash($novaSenhaPura, PASSWORD_DEFAULT);
-
-// Atualiza o banco
-$sql = "UPDATE usuarios SET senha = ? WHERE email = ?";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$hash, $email]);
-
-if ($stmt->rowCount() > 0) {
-    echo "Sua nova senha temporária é: <b>$novaSenhaPura</b>";
-    echo "<br>Faça login e altere-a imediatamente.";
-} else {
-    echo "<script>alert('E-mail não encontrado!');</script>";
-}*/
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    print_r($email);
+}
 include 'header.php';
 ?>
     <main class="main">
